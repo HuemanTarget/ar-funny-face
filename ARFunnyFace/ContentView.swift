1abe61c5f3c86e203d570dba809f72c9de9312dc
Joshua Basche <joshbasche@live.com>
2021-09-13 08:09:02 -0700

    add button images to Assets

--- ARFunnyFace/ContentView.swift
+++ ARFunnyFace/ContentView.swift
@@ -9,35 +9,47 @@ import SwiftUI
 import RealityKit
 
 struct ContentView : View {
-    var body: some View {
-        return ARViewContainer().edgesIgnoringSafeArea(.all)
+  @State var propId: Int = 0
+  
+  var body: some View {
+    
+    ZStack(alignment: .bottom) {
+      ARViewContainer(propId: $propId).edgesIgnoringSafeArea(.all)
+      
+      HStack {
+        
+      }
     }
+   
+  }
 }
 
 struct ARViewContainer: UIViewRepresentable {
+  @Binding var propId: Int
+  
+  
+  func makeUIView(context: Context) -> ARView {
     
-    func makeUIView(context: Context) -> ARView {
-        
-        let arView = ARView(frame: .zero)
-        
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
-        
-        return arView
-        
-    }
+    let arView = ARView(frame: .zero)
     
-    func updateUIView(_ uiView: ARView, context: Context) {}
+    // Load the "Box" scene from the "Experience" Reality File
+    //        let boxAnchor = try! Experience.loadBox()
     
+    // Add the box anchor to the scene
+    //        arView.scene.anchors.append(boxAnchor)
+    
+    return arView
+    
+  }
+  
+  func updateUIView(_ uiView: ARView, context: Context) {}
+  
 }
 
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
 #endif
